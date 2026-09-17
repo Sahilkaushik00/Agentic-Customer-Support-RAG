d80a0c52fdc770b61f1e6f8d453deb726a09a7e1
Sahil Kaushik <122218033+Sahilkaushik00@users.noreply.github.com>
2026-09-17 15:35:48 +0530

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -336,18 +336,12 @@ class FallbackClassifier:
 
 if SKLEARN_AVAILABLE:
     classes = sorted(merged['intent'].unique())
-    stratify = (
-    merged["intent"]
-    if merged["intent"].value_counts().min() >= 2
-    else None
-    )
-    
     X_train, X_val, y_train, y_val = train_test_split(
-    merged["user_text"],
-    merged["intent"],
+    merged['user_text'],
+    merged['intent'],
     test_size=0.2,
     random_state=42,
-    stratify=stratify
+    stratify=merged['intent']
     )
     classifier = Pipeline([
         ('tfidf', TfidfVectorizer(
